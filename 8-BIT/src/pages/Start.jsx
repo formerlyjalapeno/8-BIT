930baa78192fc4d83f4f66572d6ec316f8f94466
Jowielynne Hoogenstrijd <9023130@student.zadkine.nl>
2024-11-20 20:45:00 +0100

    START

---
+++ 8-BIT/src/pages/Start.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+const Start = () => {
+  return (
+    <>
+      <div className="start__button">
+        <div className="start__text">
+          <button>START</button>
+          <span>START</span>
+        </div>
+        <p>today, at 0:00 AM</p>
+      </div>
+    </>
+  );
+};
+
+export default Start;
