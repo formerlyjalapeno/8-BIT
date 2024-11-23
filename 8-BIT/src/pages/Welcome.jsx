1bbdf017c54a45637f93a3c3d45c2a20e3466cd9
SOVZDEV <9023130@student.zadkine.nl>
2024-11-23 14:04:55 +0100

    WelcomePage | Not Finished.

--- 8-BIT/src/pages/Welcome.jsx
+++ 8-BIT/src/pages/Welcome.jsx
@@ -6,7 +6,7 @@ const Welcome = () => {
                     <h1>WELCOME</h1>
                     <div className="double__Text">
                         <h2>TO</h2>
-                        <div className="8BIT">
+                        <div className="bit">
                             <h3>8-BIT</h3>
                             <span>8-BIT</span>
                         </div>
