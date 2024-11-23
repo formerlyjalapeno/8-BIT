3b3097bbf3e7c7065c15e5541f1b4a4701120168
SOVZDEV <9023130@student.zadkine.nl>
2024-11-23 13:39:27 +0100

    CSS to SCSS

--- 8-BIT/src/App.jsx
+++ 8-BIT/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.scss";
 import Start from "./pages/Start";
+import Welcome from "./pages/Welcome"
 
 function App() {
   return (
     <>
-    <Start />
+    <Welcome />
     </>
   );
 }
--- 8-BIT/src/pages/Welcome.jsx
+++ 8-BIT/src/pages/Welcome.jsx
@@ -1,7 +1,18 @@
 const Welcome = () => {
     return ( 
         <main className="welcome__Container">
-            
+            <section>
+                <article className="welcome__Message">
+                    <h1>WELCOME</h1>
+                    <div className="double__Text">
+                        <h2>TO</h2>
+                        <div className="8BIT">
+                            <h3>8-BIT</h3>
+                            <span>8-BIT</span>
+                        </div>
+                    </div>
+                </article>
+            </section>
         </main>
      );
 }
