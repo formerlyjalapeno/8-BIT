cd337b673ae07c95d7ff7227535eeea74c55918b
SOVZDEV <9023130@student.zadkine.nl>
2024-11-26 11:47:52 +0100

    MORE | CREDITS, ALMOST DONE.

--- 8-BIT/src/pages/More.jsx
+++ 8-BIT/src/pages/More.jsx
@@ -1,47 +1,74 @@
+import { Link } from "react-router-dom";
+
 const More = () => {
-    return (
-      <main className="more__Container">
+  return (
+    <main className="more__Container">
+      <div className="creators__Text">
         <h1>CREATORS</h1>
         <p>made by yours truly,</p>
-        <section className="creators__section">
-          <article className="creator">
-            <h2>@NOVA</h2>
-            <div className="socials"></div>
-          </article>
-          <article className="creator">
-            <h2>@SOVZ</h2>
-            <div className="socials"></div>
-          </article>
-        </section>
-        <section className="credits__Container">
-          <h2>CREDITS</h2>
-          <article className="music__Credits">
-            <h3>MUSIC</h3>
-            <div className="music__Container">
-              <p>Lorem</p>
-              <p>Lorem</p>
-              <p>Lorem</p>
-              <p>Lorem</p>
-              <p>Lorem</p>
-              <p>Lorem</p>
-            </div>
-          </article>
-          <article className="art__Credits">
-            <h3>ART</h3>
-            <div className="art__Container">
-              <p>Lorem</p>
-              <p>Lorem</p>
-              <p>Lorem</p>
-              <p>Lorem</p>
-              <p>Lorem</p>
-              <p>Lorem</p>
-            </div>
-          </article>
-        </section>
+      </div>
+      <section className="creators__section">
+        <article className="creator">
+          <div className="nova">
+            <h3>@NOVA</h3>
+            <span>@NOVA</span>
+          </div>
+          <div className="socials">
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+          </div>
+        </article>
+        <div className="separation__line"></div>
+        <article className="creator">
+          <div className="sovz">
+            <h3>@SOVZ</h3>
+            <span>@SOVZ</span>
+          </div>
+          <div className="socials">
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+          </div>
+        </article>
+      </section>
+      <section className="credits__Container">
+        <h2>CREDITS</h2>
+        <article className="music__Credits">
+          <h3>MUSIC</h3>
+          <div className="music__Container">
+            <p>Lorem</p>
+            <p>Lorem</p>
+            <p>Lorem</p>
+            <p>Lorem</p>
+            <p>Lorem</p>
+            <p>Lorem</p>
+          </div>
+        </article>
+        <article className="art__Credits">
+          <h3>ART</h3>
+          <div className="art__Container">
+            <p>Lorem</p>
+            <p>Lorem</p>
+            <p>Lorem</p>
+            <p>Lorem</p>
+            <p>Lorem</p>
+            <p>Lorem</p>
+          </div>
+        </article>
+      </section>
+      <div className="copyright__Text">
         <p>All rights reserved.</p>
         <p>© 8BIT</p>
-      </main>
-    );
+      </div>
+      <Link to="/welcome" style={{ textDecoration: "none" }}>
+        <div className="back__Button">
+          <h4>BACK</h4>
+          <span>BACK</span>
+        </div>
+      </Link>
+    </main>
+  );
 }
- 
+
 export default More;
