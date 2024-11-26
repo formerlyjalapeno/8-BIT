e1b4581b597b2340c647370a70707906caddbbd8
pascal-vegt <stefanpftf@gmail.com>
2024-11-26 11:48:50 +0100

    error

--- 8-BIT/src/components/Error.jsx
+++ 8-BIT/src/components/Error.jsx
@@ -1,8 +1,20 @@
 const Error = () => {
-    return ( 
-        <>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <section className="Error__Box">
+        <img className="Error__Box__Image" src="" alt="" />
+        <article className="Error__Box__Container">
+          <p className="Error__Box__Container__Text">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta
+            non, accusantium, et mollitia, amet autem similique magnam vitae
+            ipsa distinctio dolorem. At temporibus ipsam consequuntur nihil. Est
+            autem minus reiciendis!
+          </p>
+        </article>
+        <div className="Error__Box__Close-button">x</div>
+      </section>
+    </>
+  );
+};
+
 export default Error;
