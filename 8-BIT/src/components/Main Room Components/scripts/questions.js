b878da15f48ec19693ff88d272d8675927f3c117
Jowielynne Hoogenstrijd <9023130@student.zadkine.nl>
2025-01-12 02:16:03 +0100

    answer box fixedd

--- 8-BIT/src/components/Main Room Components/scripts/questions.js
+++ 8-BIT/src/components/Main Room Components/scripts/questions.js
@@ -2,7 +2,7 @@
 const Questions = [
   {
     id: 0,
-    title: "question_1",
+    title: "QUESTION NO.1:",
     text: "01001110 01001001 01001110 01010100 01000101 01001110 01000100 01001111",
     class: "room__container__question__overlay__content__question__1",
     requiresAnswer: true,
@@ -10,14 +10,14 @@ const Questions = [
   },
   {
     id: 1,
-    title: "question_2",
+    title: "QUESTION NO.2:",
     text: "Here is another question.",
     class: "room__container__question__overlay__content__question__2",
     requiresAnswer: false,
   },
   {
     id: 2,
-    title: "question_3",
+    title: "QUESTION NO.3:",
     text: "What do you think about this?",
     class: "room__container__question__overlay__content__question__3",
     requiresAnswer: false,
